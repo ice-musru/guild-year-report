7071b4df87cc99a309061f25883cd5614fc732ed
墨苏 <921371286@qq.com>
2021-12-31 15:20:15 +0800

    clean

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,17 @@ import "swiper/dist/css/swiper.css";
 import "@/assets/style/main.scss";
 
 import { request } from "@/utils/request";
-// import { isDev } from "@/utils/env";
+import { isDev } from "@/utils/env";
 
 const getGuildReport = async () => {
   const result = await request.get("/xlive/mcn-interface/v1/guild/GuildAnnualReport", {});
   await store.dispatch("setGuildData", result.data);
 };
 
-const vConsole = new VConsole();
-console.log(vConsole, "vConsole");
+if (isDev) {
+  const vConsole = new VConsole();
+  console.log(vConsole, "vConsole");
+}
 
 const render = async () => {
   const app = createApp(App);
